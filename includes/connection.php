a82321e1894f0a4114329fcc551ce4eaa6b24032
wosa.krishna <wosa.krishna@gmail.com>
2021-05-20 15:48:03 +0530

    validation

--- includes/connection.php
+++ includes/connection.php
@@ -1,3 +1,3 @@
 <?php
-	$con = mysqli_connect("localhost", "root", "", "basics");
-	//$con = mysqli_connect("sql6.freemysqlhosting.net", "sql6413040", "6K6cBp9dkK", "sql6413040");
+	//$con = mysqli_connect("localhost", "root", "", "basics");
+	$con = mysqli_connect("sql6.freemysqlhosting.net", "sql6413040", "6K6cBp9dkK", "sql6413040");
